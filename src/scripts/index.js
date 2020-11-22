24337553d3d7bd651fc23d4fbf22faf438c958db
brun1n0s <crinitib@gmail.com>
2020-11-22 22:37:02 +0100

    drang and drop

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,143 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+let zoom = 100;
+const buttonZoomOut = document.querySelector('#zoom-out');
+buttonZoomOut.addEventListener('click', () => {
+  zoom = zoom + 10;
+  document.querySelector('.container').style.setProperty('zoom', zoom+'%');
+});
+const buttonZoomIn = document.querySelector('#zoom-in');
+buttonZoomIn.addEventListener('click', () => {
+  zoom = zoom - 10;
+  document.querySelector('.container').style.setProperty('zoom', zoom+'%');
+});
+/*Make resizable div by Hung Nguyen*/
+function makeResizableDiv(div) {
+  const element = document.querySelector(div);
+  const resizers = document.querySelectorAll(div + ' .resizer');
+  const minimum_size = 20;
+  let original_width = 0;
+  let original_height = 0;
+  let original_x = 0;
+  let original_y = 0;
+  let original_mouse_x = 0;
+  let original_mouse_y = 0;
+  for (let i = 0;i < resizers.length; i++) {
+    const currentResizer = resizers[i];
+    currentResizer.addEventListener('mousedown', function(e) {
+      e.preventDefault();
+      original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+      original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+      original_x = element.getBoundingClientRect().left;
+      original_y = element.getBoundingClientRect().top;
+      original_mouse_x = e.pageX;
+      original_mouse_y = e.pageY;
+      window.addEventListener('mousemove', resize);
+      window.addEventListener('mouseup', stopResize);
+    });
+
+    function resize(e) {
+      if (currentResizer.classList.contains('bottom-right')) {
+        const width = original_width + (e.pageX - original_mouse_x);
+        const height = original_height + (e.pageY - original_mouse_y);
+        if (width > minimum_size) {
+          element.style.width = width + 'px';
+        }
+        if (height > minimum_size) {
+          element.style.height = height + 'px';
+        }
+      }
+      else if (currentResizer.classList.contains('bottom-left')) {
+        const height = original_height + (e.pageY - original_mouse_y);
+        const width = original_width - (e.pageX - original_mouse_x);
+        if (height > minimum_size) {
+          element.style.height = height + 'px';
+        }
+        if (width > minimum_size) {
+          element.style.width = width + 'px';
+          element.style.left = original_x + (e.pageX - original_mouse_x) + 'px';
+        }
+      }
+      else if (currentResizer.classList.contains('top-right')) {
+        const width = original_width + (e.pageX - original_mouse_x);
+        const height = original_height - (e.pageY - original_mouse_y);
+        if (width > minimum_size) {
+          element.style.width = width + 'px';
+        }
+        if (height > minimum_size) {
+          element.style.height = height + 'px';
+          element.style.top = original_y + (e.pageY - original_mouse_y) + 'px';
+        }
+      }
+      else {
+        const width = original_width - (e.pageX - original_mouse_x);
+        const height = original_height - (e.pageY - original_mouse_y);
+        if (width > minimum_size) {
+          element.style.width = width + 'px';
+          element.style.left = original_x + (e.pageX - original_mouse_x) + 'px';
+        }
+        if (height > minimum_size) {
+          element.style.height = height + 'px';
+          element.style.top = original_y + (e.pageY - original_mouse_y) + 'px';
+        }
+      }
+    }
+
+    function stopResize() {
+      window.removeEventListener('mousemove', resize);
+    }
+  }
+}
+
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+
+function dragStart() {
+  this.className += ' hold';
+  setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+  this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  this.className = 'empty';
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+}
+
+
+makeResizableDiv('#div-0');
+makeResizableDiv('#div-1');
+makeResizableDiv('#div-2');
+makeResizableDiv('#div-3');
+makeResizableDiv('#div-4');
